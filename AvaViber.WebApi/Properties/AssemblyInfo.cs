95a8b74564173dfceeb31d4495af236dec9b845c
try1975 <try1975@mail.ru>
2019-05-31 15:25:49 +0300

    Add project files.

---
+++ AvaViber.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AvaViber.WebApi.Template")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AvaViber.WebApi.Template")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1dec5fb5-07fd-4708-85c3-6a18caf6ad1d")]
+
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
---
+++ AvaViber.WebApi/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using AvaViber.WebApi.App_Start;
+
+[assembly: OwinStartup(typeof(AvaViber.WebApi.Template.Startup))]
+namespace AvaViber.WebApi.Template
+{
+    /// <summary>
+    /// Represents the entry point into an application.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Specifies how the ASP.NET application will respond to individual HTTP request.
+        /// </summary>
+        /// <param name="app">Instance of <see cref="IAppBuilder"/>.</param>
+        public void Configuration(IAppBuilder app)
+        {
+            new ApiConfig(app)
+                .ConfigureCorsMiddleware(ConfigurationManager.AppSettings["cors"])
+                .ConfigureAufacMiddleware()
+                .ConfigureFormatters()
+                .ConfigureRoutes()
+                .ConfigureExceptionHandling()
+                .ConfigureSwagger()
+                .UseWebApi();
+        }
+    }
+}
